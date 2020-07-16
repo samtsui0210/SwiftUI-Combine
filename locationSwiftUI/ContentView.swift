a2af82f9d952c5e32caa0286dd979fa98af8a7f1
SamTsui <samtsui@likon.com.hk>
2020-07-16 11:09:23 +0800

    Initial Commit

---
+++ locationSwiftUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  locationSwiftUI
+//
+//  Created by TsuiWingLok on 16/7/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ locationSwiftUI/locationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  locationSwiftUIApp.swift
+//  locationSwiftUI
+//
+//  Created by TsuiWingLok on 16/7/2020.
+//
+
+import SwiftUI
+
+@main
+struct locationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
